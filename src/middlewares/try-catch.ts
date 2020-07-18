fd88953f3aeaa3dbd3e77894103e96c89e2ab0ea
seungbin <tanja1206@naver.com>
2020-07-18 20:08:20 +0900

    try catch middleware

---
+++ src/middlewares/try-catch.ts
@@ -0,0 +1,41 @@
+import { Request, Response, NextFunction } from "express";
+
+interface handlerFunc {
+    (req: Request, res: Response, next: NextFunction): void;
+}
+
+export class tryCatchMiddleware {
+    static NotFound = (cb: handlerFunc): handlerFunc => {
+        return async (req: Request, res: Response, next: NextFunction) => {
+            try {
+                await cb(req, res, next);
+            } catch (e) {
+                res.status(404).json({
+                    message: e.message
+                })
+            }
+        }
+    }
+    static Conflict = (cb: handlerFunc): handlerFunc => {
+        return async (req: Request, res: Response, next: NextFunction) => {
+            try {
+                await cb(req, res, next);
+            } catch (e) {
+                res.status(409).json({
+                    message: e.message
+                })
+            }
+        }
+    }
+    static ServerError = (cb: handlerFunc): handlerFunc => {
+        return async (req: Request, res: Response, next: NextFunction) => {
+            try {
+                await cb(req, res, next);
+            } catch (e) {
+                res.status(500).json({
+                    message: e.message
+                })
+            }
+        }
+    }
+}
