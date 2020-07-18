9db0849907ccaf1727289304acc7a3dd1757a65b
seungbin <tanja1206@naver.com>
2020-07-18 23:30:34 +0900

    main

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response, NextFunction, urlencoded } from "express";
+import express, { Request, Response, NextFunction } from "express";
 import { sequelize } from "./config/config";
 import morgan from "morgan";
 import router from "./routes"
