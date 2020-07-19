34c1d503ce1a8ffec5f47c34171c60e4cb35230a
seungbin <tanja1206@naver.com>
2020-07-19 14:00:20 +0900

    add relational

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,10 +1,13 @@
 import { sequelize } from "../config/config";
 import Sequelize, { Model } from "sequelize";
+import { Post } from "./post";
 
 export class User extends Model {
     name: string;
     username: string;
     password: string;
+    accessToken: string;
+    refreshToken: string;
 }
 
 User.init (
@@ -21,9 +24,18 @@ User.init (
         password: {
             type: Sequelize.STRING,
             allowNull: false
+        },
+        accessToken: {
+            type: Sequelize.STRING,
+        },
+        refreshToken: {
+            type: Sequelize.STRING,
         }
     }, {
         sequelize,
         modelName: "User"
     }
-)
+);
+
+User.hasMany(Post, { foreignKey: "username", sourceKey: "username" });
+Post.belongsTo(User, { foreignKey: "username" });
