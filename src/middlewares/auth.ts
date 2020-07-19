00cff415d2fee464f9a7357a2023745fa18c3f77
seungbin <tanja1206@naver.com>
2020-07-19 13:59:51 +0900

    token check middleware

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,13 +1,13 @@
 import jwt from "jsonwebtoken";
 import { Request, Response, NextFunction } from "express";
 
-const authMiddleware = async (req: Request, res: Response, next: NextFunction) => {
+const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
     const token: any = req.headers["access-token"];
-    if (!token) res.status(403).json({ message: "로그인 되어있지 않음" });
-    await jwt.verify(token, req.app.get('jwt-secret'), (err, decoded) => {
-        if (err) throw new Error("에러");
+    if (!token) return res.status(403).json({ message: "로그인 되어있지 않음" });
+    jwt.verify(token, req.app.get('jwt-secret'), (err, decoded) => {
+        if (err) return res.status(403).json({ message: err.mesasge });
         req["decoded"] = decoded;
-        next();
+        next()
     })
 }
 
