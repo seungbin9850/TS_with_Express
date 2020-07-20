29062a6a692e39167da2b7ed29e676026d363cd3
seungbin <tanja1206@naver.com>
2020-07-20 14:38:45 +0900

    token check middleware

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -5,7 +5,7 @@ const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
     const token: any = req.headers["access-token"];
     if (!token) return res.status(403).json({ message: "로그인 되어있지 않음" });
     jwt.verify(token, req.app.get('jwt-secret'), (err, decoded) => {
-        if (err) return res.status(403).json({ message: err.mesasge });
+        if (err) return res.status(403).json({ message: "로그인 되어있지 않음" });
         req["decoded"] = decoded;
         next()
     })
