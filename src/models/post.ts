1393410b08d10b9380e66ef836ea5d571f7ab907
seungbin <tanja1206@naver.com>
2020-07-20 15:34:27 +0900

    post model

--- src/models/post.ts
+++ src/models/post.ts
@@ -2,12 +2,17 @@ import { sequelize } from "../config/config";
 import Sequelize, { Model } from "sequelize";
 
 export class Post extends Model {
+    id: string;
     title: string;
     content: string;
 }
 
 Post.init (
     {
+        id: {
+            type: Sequelize.STRING,
+            primaryKey: true
+        },
         title: {
             type: Sequelize.STRING
         },
