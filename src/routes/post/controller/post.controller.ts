6b26939c810b06ac2c2150d22542f74a32ce1645
seungbin <tanja1206@naver.com>
2020-07-20 15:34:42 +0900

    post controller

--- src/routes/post/controller/post.controller.ts
+++ src/routes/post/controller/post.controller.ts
@@ -1,25 +1,22 @@
 import { Request, Response, NextFunction } from "express";
-import { create, findUserByToken, deleteOnePost, compareUser, findOne } from "./query";
+import * as query from "./query";
 
-const write = async (req: Request, res: Response, next: NextFunction) => {
+export const write = async (req: Request, res: Response, next: NextFunction) => {
     const { title, content } = req.body;
     const token: any = req.headers["access-token"];
-    const user: any = await findUserByToken(token)
+    const user: any = await query.findUserByToken(token)
+    const id: any = await query.mkid();
     if (!user) throw new Error("존재하지 않는 유저");
-    await create(title, content, user.username);
+    await query.create(id, title, content, user.username);
     res.status(200).json({ message: "성공" });
 }
 
-const deleteOne = async (req: Request, res: Response, next: NextFunction) => {
+export const deleteOne = async (req: Request, res: Response, next: NextFunction) => {
     const id = req.params.id;
     const username = req["decoded"].username;
-    const post: any = await findOne(id);
+    const post: any = await query.findOne(id);
     if (!post) throw new Error("존재하지 않는 글");
-    if (!compareUser(username, post.username)) {
-        res.status(403).json({ message: "자신의 글이 아님" });
-    }
-    await deleteOnePost(id);
+    if (username !== post.username) throw new Error("자신의 글이 아님")
+    await query.deleteOnePost(id);
     res.status(200).json({ message: "성공" });
 }
-
-export { write, deleteOne };
