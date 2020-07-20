56e9069218c1a8d80a0d267112a0572a04f24cf1
seungbin <tanja1206@naver.com>
2020-07-20 14:38:37 +0900

    delete post

--- src/routes/post/controller/post.controller.ts
+++ src/routes/post/controller/post.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response, NextFunction } from "express";
-import { create, findUserByToken, deleteOnePost } from "./query";
+import { create, findUserByToken, deleteOnePost, compareUser, findOne } from "./query";
 
 const write = async (req: Request, res: Response, next: NextFunction) => {
     const { title, content } = req.body;
@@ -12,6 +12,12 @@ const write = async (req: Request, res: Response, next: NextFunction) => {
 
 const deleteOne = async (req: Request, res: Response, next: NextFunction) => {
     const id = req.params.id;
+    const username = req["decoded"].username;
+    const post: any = await findOne(id);
+    if (!post) throw new Error("존재하지 않는 글");
+    if (!compareUser(username, post.username)) {
+        res.status(403).json({ message: "자신의 글이 아님" });
+    }
     await deleteOnePost(id);
     res.status(200).json({ message: "성공" });
 }
