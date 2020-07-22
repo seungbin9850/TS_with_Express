571e6176e0498f10520b24e8b285d20e5669c211
seungbin <tanja1206@naver.com>
2020-07-22 21:36:25 +0900

    connect router

--- src/routes/post/index.ts
+++ src/routes/post/index.ts
@@ -1,13 +1,15 @@
 import express from "express";
 import { tryCatchMiddleware } from "../../middlewares/try-catch";
 import * as auth from "../../middlewares/auth";
-import { write, deleteOne, updateOne } from "./controller/post.controller";
+import * as post  from "./controller/post.controller";
 
 const router = express.Router();
 const accessToken = auth.authMiddleware;
 
-router.post('/', accessToken, tryCatchMiddleware.NotFound(write));
-router.delete('/:id', accessToken, tryCatchMiddleware.NotFound(deleteOne));
-router.put('/:id', accessToken, tryCatchMiddleware.NotFound(updateOne));
+router.post('/', accessToken, tryCatchMiddleware.NotFound(post.write));
+router.delete('/:id', accessToken, tryCatchMiddleware.NotFound(post.deleteOne));
+router.put('/:id', accessToken, tryCatchMiddleware.NotFound(post.updateOne));
+router.get('/', tryCatchMiddleware.NotFound(post.getAll));
+router.get('/:id', tryCatchMiddleware.NotFound(post.getOne));
 
 export default router;
