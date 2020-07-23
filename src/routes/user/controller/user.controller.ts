344b3fcbc5b7fe5c19edc54d3af065e2f8244647
seungbin <tanja1206@naver.com>
2020-07-23 19:04:43 +0900

    user controller

--- src/routes/user/controller/user.controller.ts
+++ src/routes/user/controller/user.controller.ts
@@ -1,22 +1,22 @@
 import { Request, Response, NextFunction } from "express";
-import { findOneByUsername, createUser, passwordCompare, passwordHashing, findOneByRefresh } from "./query";
+import * as query from "./query";
 import { mkAccess, mkRefresh } from "./mkToken";
 
 
 export const signUp = async (req: Request, res: Response, next: NextFunction) => {
     const { name, username, password } = req.body;
-    const hashedPassword = await passwordHashing(password);
-    if (await findOneByUsername(username)) throw new Error("이미 있는 아이디");
-    await createUser(name, username, hashedPassword);
+    const hashedPassword = await query.passwordHashing(password);
+    if (await query.findOneByUsername(username)) throw new Error("이미 있는 아이디");
+    await query.createUser(name, username, hashedPassword);
     res.status(200).json({ message: "회원가입 성공" });
 };
 
 export const login = async (req: Request, res: Response, next: NextFunction) => {
     const { username, password } = req.body;
-    const user = await findOneByUsername(username);
+    const user = await query.findOneByUsername(username);
 
     if (!user) throw new Error("아이디 혹은 비밀번호가 틀림");
-    if (!await passwordCompare(password, user.password)) throw new Error("아이디 혹은 비밀번호가 틀림");
+    if (!await query.passwordCompare(password, user.password)) throw new Error("아이디 혹은 비밀번호가 틀림");
     const accessToken = await mkAccess(req, user);
     const refreshToken = await mkRefresh(req, user);
     user.accessToken = accessToken;
@@ -31,7 +31,7 @@ export const login = async (req: Request, res: Response, next: NextFunction) => 
 
 export const refresh = async (req: Request, res: Response, next: NextFunction) => {
     const refresh: any = req.headers["refresh-token"]
-    const user: any = await findOneByRefresh(refresh);
+    const user: any = await query.findOneByRefresh(refresh);
     const accessToken = await mkAccess(req, user);
     user.accessToken = accessToken;
     user.save();
