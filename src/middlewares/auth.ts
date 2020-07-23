5a1ab4bac20bca393dd16110b4ded6253e5f7dfc
seungbin <tanja1206@naver.com>
2020-07-23 12:59:39 +0900

    token check

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,14 +1,21 @@
 import jwt from "jsonwebtoken";
 import { Request, Response, NextFunction } from "express";
 
-const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
+export const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
     const token: any = req.headers["access-token"];
     if (!token) return res.status(403).json({ message: "로그인 되어있지 않음" });
     jwt.verify(token, req.app.get('jwt-secret'), (err, decoded) => {
         if (err) return res.status(403).json({ message: "로그인 되어있지 않음" });
         req["decoded"] = decoded;
-        next()
+        next();
     })
 }
 
-export { authMiddleware }
+export const refreshMiddleware = (req: Request, res: Response, next: NextFunction) => {
+    const token: any = req.headers["refresh-token"];
+    if (!token) return res.status(403).json({ message: "로그인 되어있지 않음" });
+    jwt.verify(token, req.app.get('refresh-secret'), (err) => {
+        if (err) return res.status(403).json({ message: "로그인 되어있지 않음" });
+        next();
+    })
+}
