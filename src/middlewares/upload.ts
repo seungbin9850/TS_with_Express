a32d0bd2a801ad6afaba645c78961266c983a4fc
seungbin <tanja1206@naver.com>
2020-07-24 15:07:00 +0900

    file upload middleware

---
+++ src/middlewares/upload.ts
@@ -0,0 +1,14 @@
+import multer from "multer";
+import path from "path";
+
+export const uploadMiddleware = multer({
+    storage: multer.diskStorage({
+        destination(req, file, cb) {
+            cb(null, 'uploads/');
+        },
+        filename(req, file, cb) {
+            const ext = path.extname(file.originalname);
+            cb(null, path.basename(file.originalname, ext) + Date.now() + ext);
+        }
+    })
+});
