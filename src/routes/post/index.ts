4377e0d5afa33ba53ba710d38e9bd1f96bfe9357
seungbin <tanja1206@naver.com>
2020-07-24 15:07:47 +0900

    connect router

--- src/routes/post/index.ts
+++ src/routes/post/index.ts
@@ -1,12 +1,13 @@
 import express from "express";
 import { tryCatchMiddleware } from "../../middlewares/try-catch";
+import { uploadMiddleware } from "../../middlewares/upload";
 import * as auth from "../../middlewares/auth";
 import * as post  from "./controller/post.controller";
 
 const router = express.Router();
 const accessToken = auth.authMiddleware;
 
-router.post('/', accessToken, tryCatchMiddleware.NotFound(post.write));
+router.post('/', accessToken, uploadMiddleware.single('file'), tryCatchMiddleware.NotFound(post.write));
 router.delete('/:id', accessToken, tryCatchMiddleware.NotFound(post.deleteOne));
 router.put('/:id', accessToken, tryCatchMiddleware.NotFound(post.updateOne));
 router.get('/', tryCatchMiddleware.NotFound(post.getAll));
