fe21928b2f596607ab39aa4726b771aa52c31ab5
seungbin <tanja1206@naver.com>
2020-07-27 22:46:36 +0900

    upload middleware s3

--- src/middlewares/upload.ts
+++ src/middlewares/upload.ts
@@ -1,14 +1,24 @@
 import multer from "multer";
+import multerS3 from "multer-s3";
+import aws from "aws-sdk";
 import path from "path";
+import dotenv from "dotenv";
+dotenv.config({ path: path.join(__dirname, '../../.env') });
+
+const s3 = new aws.S3({
+    accessKeyId: process.env.S3_ACCESS,
+    secretAccessKey: process.env.S3_SECRET,
+    region: "ap-northeast-2"
+});
 
 export const uploadMiddleware = multer({
-    storage: multer.diskStorage({
-        destination(req, file, cb) {
-            cb(null, 'uploads/');
+    storage: multerS3({
+        s3: s3,
+        bucket: process.env.S3_NAME,
+        key: (req, file, cb) => {
+            const extention = path.extname(file.originalname);
+            cb(null, Date.now().toString() + extention);
         },
-        filename(req, file, cb) {
-            const ext = path.extname(file.originalname);
-            cb(null, path.basename(file.originalname, ext) + Date.now() + ext);
-        }
-    })
-});
+        acl: "public-read-write"
+    }) 
+})
