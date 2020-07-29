8ff5a0634fbfe937c2c6095170cfc4078de9e265
seungbin <tanja1206@naver.com>
2020-07-29 16:40:14 +0900

    post controller

--- src/routes/post/controller/post.controller.ts
+++ src/routes/post/controller/post.controller.ts
@@ -8,15 +8,15 @@ export const write = async (req: Request, res: Response, next: NextFunction) => 
     const user: any = await query.findUserByToken(token);
     const id: string = await query.mkid();
     if (!user) throw new Error("존재하지 않는 유저");
-    await query.create(id, title, content, user.username, file);
+    await query.create(id, title, content, user.id, file);
     res.status(200).json({ message: "성공" });
 }
 
 export const deleteOne = async (req: Request, res: Response, next: NextFunction) => {
     const id = req.params.id;
-    const username = req["decoded"].username;
+    const userId = req["decoded"].id;
     const post: any = await query.findOne(id);
-    if (username !== post.username) throw new Error("자신의 글이 아님")
+    if (userId !== post.userId) throw new Error("자신의 글이 아님")
     await query.deleteOnePost(id);
 
     res.status(200).json({ message: "성공" });
@@ -25,9 +25,9 @@ export const deleteOne = async (req: Request, res: Response, next: NextFunction)
 export const updateOne = async (req: Request, res: Response, next: NextFunction) => {
     const id = req.params.id;
     const { title, content, file } = req.body;
-    const username = req["decoded"].username;
+    const userId = req["decoded"].id;
     const post: any = await query.findOne(id);
-    if (username !== post.username) throw new Error("자신의 글이 아님");
+    if (userId !== post.username) throw new Error("자신의 글이 아님");
     await query.updateOnePost(id, title, content, file);
     res.status(200).json({ message: "성공" });
 }
